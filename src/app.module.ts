3f867ebb84c477d6123292fee79e7866136372b9
kkkiikkk <ruddnovskiy@gmail.com>
2022-09-11 23:14:52 +0300

    lint

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,20 +3,14 @@ import { Module } from '@nestjs/common';
 
 // Modules
 import { MongoModule } from './mongo/mongo.module';
-import { DataServiceModule } from './bus/services/data.service.module'
-import { CategoryServiceModule } from './bus/services/category/category-service.module'
+import { DataServiceModule } from './bus/services/data.service.module';
+import { CategoryServiceModule } from './bus/services/category/category-service.module';
 
 // Controllers
-import { CategoryController } from './bus/controllers/category.controller'
+import { CategoryController } from './bus/controllers/category.controller';
 
 @Module({
-  imports: [
-    MongoModule,
-    DataServiceModule,
-    CategoryServiceModule
-  ],
-  controllers: [
-    CategoryController
-  ]
+  imports: [MongoModule, DataServiceModule, CategoryServiceModule],
+  controllers: [CategoryController],
 })
 export class AppModule {}
--- src/bus/controllers/category.controller.ts
+++ src/bus/controllers/category.controller.ts
@@ -1,34 +1,34 @@
 // Core
-import { Controller, Get, Param, Put, Body, Post } from '@nestjs/common'
+import { Controller, Get, Param, Put, Body, Post } from '@nestjs/common';
 
 // Tools
-import { UpdateCategoryDto, CreateCategoryDto } from '../../interfaces'
-import { CategoryService } from '../services/category/category.service'
+import { UpdateCategoryDto, CreateCategoryDto } from '../../interfaces';
+import { CategoryService } from '../services/category/category.service';
 
 @Controller('api')
 export class CategoryController {
-  constructor( private categoryService: CategoryService) {}
+  constructor(private categoryService: CategoryService) {}
 
   @Get('/categories')
   async getAll() {
-    return this.categoryService.getAllCategory()
+    return this.categoryService.getAllCategory();
   }
 
   @Get('/categories/:id')
   async getById(@Param('id') id: string) {
-   return this.categoryService.getCategoryById(id)
+    return this.categoryService.getCategoryById(id);
   }
 
   @Post('/category')
   async createCategory(@Body() createCategoryDto: CreateCategoryDto) {
-    return this.categoryService.createCategory(createCategoryDto)
+    return this.categoryService.createCategory(createCategoryDto);
   }
 
   @Put('/categories/:id')
   async updateCategory(
     @Param('id') categoryId: string,
-    @Body() updateCategoryDto: UpdateCategoryDto
+    @Body() updateCategoryDto: UpdateCategoryDto,
   ) {
-    return this.categoryService.updateCategory(categoryId, updateCategoryDto)
+    return this.categoryService.updateCategory(categoryId, updateCategoryDto);
   }
 }
